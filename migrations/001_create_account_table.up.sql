339210e4a8aef6fd6a92cf26595572f5a1d535d1
Николай Сафонов <uporoty-cot@mail.ru>
2026-07-23 20:57:34 +0300

    add api spec for account api

--- migrations/001_create_account_table.up.sql
+++ migrations/001_create_account_table.up.sql
@@ -1,5 +1,6 @@
 create table if not exists account (
     id uuid primary key,
+    idempotency_key uuid not null,
     metadata jsonb not null,
     status text not null,
     created_at timestamp with time zone default now()
